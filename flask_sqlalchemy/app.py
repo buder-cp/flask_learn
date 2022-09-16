4e1a712d5cdbcd203cb9c396874a63b552c10597
chenpeng12 <chenpeng12@staff.sina.com>
2022-09-16 16:15:36 +0800

    【flask】 数据库学习，数据库连接

--- flask_sqlalchemy/app.py
+++ flask_sqlalchemy/app.py
@@ -1,10 +1,28 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+# 数据库的配置变量
+HOSTNAME = '127.0.0.1'
+PORT = '3306'
+DATABASE = 'zl_flask'
+USERNAME = 'root'
+PASSWORD = 'root'
+DB_URI = 'mysql+pymysql://{}:{}@{}:{}/{}?charset=utf8'.format(USERNAME, PASSWORD, HOSTNAME, PORT, DATABASE)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = DB_URI
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+db = SQLAlchemy(app)
 
 
 @app.route('/')
 def hello_world():  # put application's code here
+    engine = db.get_engine()
+    with engine.connect() as conn:
+        result = conn.execute("select 1")
+        print(result.fetchone())
     return 'Hello World!'
 
 
